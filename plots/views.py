ba9576f056b999e3a039c2eea9a2440452fb1934
Matthew Dawson <mattdawson@ucar.edu>
2020-06-25 15:10:25 -0700

    setup django project and plot from matplotlib

---
+++ plots/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from matplotlib import pylab
+from pylab import *
+import PIL, PIL.Image, io
+
+def index(request):
+    matplotlib.use('agg')
+
+    x = arange(0, 2*pi, 0.01)
+    s = cos(x)**2
+    plot(x, s)
+
+    xlabel('xlabel(X)')
+    ylabel('ylabel(Y)')
+    title('Simple Graph!')
+    grid(True)
+
+    # Store image in a string buffer
+    buffer = io.BytesIO()
+    canvas = pylab.get_current_fig_manager().canvas
+    canvas.draw()
+    pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+    pilImage.save(buffer, "PNG")
+    pylab.close()
+
+    # Send buffer in a http response the the browser with the mime type image/png set
+    return HttpResponse(buffer.getvalue(), content_type="image/png")
+
